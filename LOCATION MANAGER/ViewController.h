e65eb5ea8a0e706e0b64768792187fc26552b10b
ShariqueAzam <student1@CR33S30I.local>
2014-04-27 00:35:34 +0800

    location Manager

--- LOCATION MANAGER/ViewController.h
+++ LOCATION MANAGER/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate>{
+    IBOutlet UITextView *textView;
+    CLLocationManager *locationManager;
+    NSUInteger noUpdates;
+}
 
 @end
--- LOCATION MANAGER/ViewController.m
+++ LOCATION MANAGER/ViewController.m
@@ -14,10 +14,42 @@
 
 @implementation ViewController
 
+
+
+-(void) updateLocation:(NSString*) locationDesc{
+    NSMutableString *newMessage = [[NSMutableString alloc]initWithCapacity:100];
+    NSLog(@"Inside update Location");
+    [newMessage appendString:[NSString stringWithFormat:@"Update #:%i\n",noUpdates]];
+    [newMessage appendString:locationDesc];
+    [newMessage appendString:@"\n"];
+    [newMessage appendString:[textView text]];
+    textView.text = newMessage;
+    
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    noUpdates++;
+    if (noUpdates >= 50) {
+        [locationManager stopUpdatingLocation];
+        textView.text = @"Location Manager stop running...";
+    }
+    CLLocation *lastLocation = [locations lastObject];
+    [self updateLocation:[lastLocation description]];
+}
+
+
+-(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"Could not locate location: %@",error);
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    locationManager = [[CLLocationManager alloc]init];
+    [locationManager setDelegate:self];
+    [locationManager startUpdatingLocation];
+    textView.text = @"Location Manager start running...";
 }
 
 - (void)didReceiveMemoryWarning
